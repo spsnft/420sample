7d532df7676f5bf7d9840fef9ced4bf6ba239d4f
spsnft <87615686+spsnft@users.noreply.github.com>
2026-08-24 20:05:36 +0700

    Adjust top margin class for mockup in PartnersClient

--- src/components/partners/PartnersClient.tsx
+++ src/components/partners/PartnersClient.tsx
@@ -364,7 +364,7 @@ export default function PartnersClient() {
           title={t.blockStorefrontTitle}
           subtitle={t.blockStorefrontSubtitle}
           mobileCtaGapClassName="mt-[-30px]"
-          mockupTopMarginClassName="-mt-20 lg:-mt-20"
+          mockupTopMarginClassName="-mt-20 lg:-mt-30"
           mockup={
             <PhoneMockup
               src="/images/partners/customer-view.png"
